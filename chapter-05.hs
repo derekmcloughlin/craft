e2926701381116d9e560321b1b21a953dd784b22
Derek McLoughlin <derek.mcloughlin@gmail.com>
2013-03-02 10:37:34 +0000

    more chapter 5

--- chapter-05.hs
+++ chapter-05.hs
@@ -77,7 +77,7 @@ testsRoot = TestList [testRoot1, testRoot2, testRoot3]
 data Shape = Circle Float |
              Rectangle Float Float |
              Triangle Float Float Float
-             deriving(Eq, Ord, Show)
+             deriving(Ord, Show)
 
 perimeter :: Shape -> Float
 perimeter (Circle r )           = 2 * pi * r
@@ -214,4 +214,210 @@ data Move = Rock |
 -}
 
 -- Exercise 5.10
+
+iseq_ual :: Shape -> Shape -> Bool
+iseq_ual (Circle a) (Circle b)
+     | a == b           = True
+     | -a == b          = True
+     | otherwise        = False
+
+-- could also use the 'abs' function
+iseq_ual (Rectangle a b) (Rectangle c d)
+     | abs a == abs c && abs b == abs d  = True
+     | otherwise                         = False
+     
+-- Correct way to do this using == operator
+-- Need to remove the deriving Eq in the definition of Shape above
+instance Eq Shape where 
+    (Circle x) == (Circle y)            =  abs x == abs y
+    (Rectangle a b) == (Rectangle c d)  =  abs a == abs b && abs c == abs d
+
+
+-- Exercise 5.11
+
+
+data NewShape = NewCircle Float (Float, Float) |
+                NewRectangle Float Float (Float, Float) |
+                NewTriangle Float Float Float (Float, Float)
+                deriving(Eq, Ord, Show)
+
+
+-- Exercise 5.12
+
+move :: Float -> Float -> NewShape -> NewShape
+
+move x1 y1  (NewCircle r (x, y))        = NewCircle r (x + x1, y + y1)
+move x1 y1  (NewRectangle w h (x, y))   = NewRectangle w h (x + x1, y + y1)
+move x1 y1  (NewTriangle a b c (x, y))  = NewTriangle a b c (x + x1, y + y1)
+
+-- Exercise 5.13
+
+overlapping :: NewShape -> NewShape -> Bool
+
+overlapping (NewCircle r1 (x1, y1)) (NewCircle r2 (x2, y2)) = True
+            
+
+-- Exercise 5.14
+
+data House = HouseName String |
+             HouseNumber Integer
+             deriving (Show)
+
+
+toText :: House -> String
+toText (HouseName a)    = a
+toText (HouseNumber a)  = show a
+
+data Address = Address House String String
+               deriving (Show)
+
+my_house_no = HouseNumber 123
+
+my_house = Address my_house_no "O'Connell Street" "Dublin 2"
+
+-- Exercise 5.15
+
+{-
+    ghci> [0, 0.1 .. 1]
+    [0.0,0.1,0.2,0.30000000000000004,0.4000000000000001,0.5000000000000001,0.6000000000000001,0.7000000000000001,0.8,0.9,1.0]
+
+-}
+
+-- Exercise 5.16
+
+{-
+    ghci> length [2, 3]
+    2
+    ghci> length [[2, 3]]
+    1
+    ghci> :t [[2, 3]]
+    [[2, 3]] :: Num t => [[t]]
+-}
+
+-- Exercise 5.17
+
+{-
+    ghci> [2 .. 2]
+    [2]
+    ghci> [2, 7 .. 4]
+    [2]
+    ghci> [2, 2 .. 2]
+    [2,2,2,2,2,2,2,2,2,2,............. goes on forever
+-}
+
+-- Exercise 5.18
+
+doubleAll :: [Integer] -> [Integer]
+doubleAll xs = [x * 2 | x <- xs]
+
+-- Exercise 5.19
+
+-- Take these definitions from Exercise 3.16
+offset :: Int
+offset = fromEnum 'A' - fromEnum 'a'
+
+toUpper :: Char -> Char
+toUpper ch = toEnum(fromEnum ch + offset)
+
+isLower :: Char -> Bool
+isLower ch = 'a' <= ch && ch <= 'z' 
+
+isAlpha :: Char -> Bool
+isAlpha ch = ('a' <= ch && ch <= 'z') || ('A' <= ch && ch <= 'Z') 
+
+smallToCapitals :: Char -> Char
+
+smallToCapitals ch
+        | isLower ch = toUpper ch
+        | otherwise = ch
+
+capitalize :: String -> String
+
+capitalize xs = [smallToCapitals x | x <- xs]
+
+capitalizeLetters xs = [smallToCapitals x | x <- xs, isAlpha x]
+
+-- Exercise 5.20
+
+divisors :: Integer -> [Integer]
+
+divisors n = [x | x <- [1 .. n], n `rem` x == 0]
+
+isPrime :: Integer -> Bool
+
+isPrime n = [1, n] == divisors n
+
+-- Exercise 5.21
+
+matches :: Integer -> [Integer] -> [Integer]
+
+matches n xs = [x | x <- xs, x == n]
+
+-- "elem" conflicts with Prelude function. I prefer just to give it a different name
+-- rather than hiding it.
+elem' :: Integer -> [Integer] -> Bool
+
+elem' n xs = matches n xs /= []
+
+-- Exercise 5.22
+
+onSeparateLines :: [String] -> String
+
+onSeparateLines xs = [ch | x <- xs, ch <- x ++ "\n"]
+
+{-
+    ghci> putStr $ onSeparateLines ["Hello", "World"]
+    Hello
+    World
+-}
+
+-- Exercise 5.23
+
+duplicate :: String -> Integer -> String
+
+duplicate xs n
+    | n == 0        = ""
+    | n == 1        = xs
+    | otherwise     = [x | x <- xs, c <- [1..n] ]
+
+{-  Note: the order of the conditions in the list comprehension is important:
+
+    ghci> duplicate "hello" 3
+    "hellohellohello"
+
+    Our order goes through the [1..n] first:
+
+    | otherwise     = [x | c <- [1..n], x <- xs ]
+
+    However, if we had the order in the 'otherwise' part as follows:
+
+    | otherwise     = [x | x <- xs, c <- [1..n] ]
+
+    You'd get:
+    
+    ghci> duplicate "hello" 3
+    "hhheeellllllooo"
+-}
+
+-- Exercise 5.24
+
+-- Exercise 5.25
+
+-- Exercise 5.26
+
+-- Exercise 5.27
+
+-- Exercise 5.28
+
+-- Exercise 5.29
+
+-- Exercise 5.30
+
+-- Exercise 5.31
+
+-- Exercise 5.32
+
+-- Exercise 5.33
+
+-- Exercise 5.34
 

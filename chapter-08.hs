e374017d0fc473164179c9a7f1cbf9984b32bc24
Derek McLoughlin <derek.mcloughlin@gmail.com>
2013-03-30 16:20:28 +0000

    Chapter 8

--- chapter-08.hs
+++ chapter-08.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 -- Rock Paper Scissors from Chapter 4
 
 data Move = Rock |
@@ -61,9 +63,11 @@ tournamentOutcome (as, bs) = sum [outcome a b | (a, b) <- zip as bs]
 type Strategy = [Move] -> Move
 
 beatLastMove :: Strategy
+beatLastMove [] = Rock
 beatLastMove (latest:rest) = beat latest
 
 loseLastMove :: Strategy
+loseLastMove [] = Scissors
 loseLastMove (latest:rest) = lose latest
 
 -- Exercise 8.4
@@ -173,8 +177,8 @@ addNnumbers =
             else do
                 putStr "Enter the Integer: "
                 summand <- getInt
-                recu <- add_the_numbers (n - 1)
-                return (summand + recu)
+                rest <- add_the_numbers (n - 1)
+                return (summand + rest)
 
 -- Exercise 8.14
 
@@ -191,29 +195,106 @@ wc = do
 
 -- Exercise 8.15
 
-
+-- Need palindrome implementation
 
 -- Exercise 8.16
 
-
+-- More palindrome stuff.
 
 -- Exercise 8.17
 
 
+addNumbersRepeat :: IO ()
+addNumbersRepeat =
+    do
+        putStrLn "Enter the integers one by one until. To stop enter 0."
+        thesum <- add_the_numbers
+        print thesum
+    where 
+    add_the_numbers =
+        do
+            n <- getInt
+            if n == 0 then
+               return 0
+            else do
+                rest <- add_the_numbers
+                return (n + rest)
+
 
 -- Exercise 8.18
 
+addNumbersSort :: IO ()
+addNumbersSort =
+    do
+        putStrLn "Enter the integers one by one until. To stop enter 0."
+        numbers <- add_the_numbers
+        putStrLn "The sorted numbers are:"
+        -- Cheating - he probably wants to do the sort inline somehow as the numbers come in.
+        print $ sort numbers
+    where 
+    add_the_numbers =
+        do
+            n <- getInt
+            if n == 0 then
+               return []
+            else do
+                rest <- add_the_numbers
+                return (n : rest)
 
 
 -- Exercise 8.19
 
-
+copy :: IO ()
+copy =
+    do
+        line <- getLine
+        let whileCopy = do
+                if (line == "")
+                then return ()
+                else do
+                    putStrLn line
+                    line <- getLine
+                    whileCopy
+        whileCopy
+               
+{-  The function never terminates unless you enter a newline at the very start.
+    This is because when whileCopy gets called again recursively, "line" is re-set
+    to the value it had outside the definition.
+-}
 
 -- Exercise 8.20
 
+step :: Strategy -> Strategy -> Tournament -> Tournament
+step strategyA strategyB (movesA, movesB) =
+     (strategyA movesB : movesA,  -- What A will play
+      strategyB movesA : movesB)  -- What B will Play
 
 
+
+playSvsS :: Strategy -> Strategy -> Integer -> Tournament
+
+playSvsS strategyA strategyB n =
+    turn ([], []) n
+    where
+    turn t m =
+        if (m <= 0) then
+            t
+        else
+            turn (strategyA (snd t) : snd t, strategyB (fst t) : fst t) (m - 1)
+          
+
+testfn :: Integer -> Integer -> Integer -> Integer
+testfn a b c =
+       if a <= 0 then
+          b
+       else
+          c
+          
+      
 -- Exercise 8.21
+
+type Strategy2 = Tournament -> Move
+
 
 
 
